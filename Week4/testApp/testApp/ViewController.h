0a0b60db60a59f30759427c4e7d01e859713c794
Gawain Bracy II <g2musicgroup@fullsail.edu>
2013-03-28 18:00:37 -0700

    Added global variables to iOS
    Signed-off-by: Gawain Bracy II <g2musicgroup@fullsail.edu>

---
+++ Week4/testApp/testApp/ViewController.h
@@ -0,0 +1,22 @@
+//
+//  ViewController.h
+//  testApp
+//
+//  Created by Gawain Bracy II on 3/28/13.
+//  Copyright (c) 2013 Gawain Bracy II. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+{
+    // global vars
+    UIStepper *stepperButtons;
+    UILabel *stepperValueLabel;
+    UILabel *loginMessage;
+    UILabel *infoLabel;
+    UITextField *userNameField;
+    
+}
+
+@end
