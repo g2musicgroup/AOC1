c96a8b292d09436867fafdeb33819e2e6b8febf3
Gawain Bracy II <g2musicgroup@fullsail.edu>
2013-04-14 18:28:21 -0700

    applied NS strings for step 2
    Signed-off-by: Gawain Bracy II <g2musicgroup@fullsail.edu>

---
+++ Week1/testApp2/testApp2/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  testApp2
+//
+//  Created by Gawain Bracy II on 4/14/13.
+//  Copyright (c) 2013 Gawain Bracy II. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
