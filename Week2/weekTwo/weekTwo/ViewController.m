fc7c34f372ecde92757264f3e4d44a94993acc46
Gawain Bracy II <g2musicgroup@fullsail.edu>
2013-04-18 20:45:54 -0700

    Added label titles and label background colors
    Signed-off-by: Gawain Bracy II <g2musicgroup@fullsail.edu>

--- Week2/weekTwo/weekTwo/ViewController.m
+++ Week2/weekTwo/weekTwo/ViewController.m
@@ -59,8 +59,39 @@
     bookTopicsLabel  = [[UILabel alloc] initWithFrame:CGRectMake(10.0f,  340.0f, 300.0f,  20.0f)];
     bookTopicsText   = [[UILabel alloc] initWithFrame:CGRectMake(10.0f,  370.0f, 300.0f,  80.0f)];
 
+    // check to make sure it was set right
+    if ((bookTitle != nil) &&
+        (bookAuthorLabel != nil) &&
+        (bookAuthorText != nil) &&
+        (bookPubLabel != nil) &&
+        (bookPubText != nil) &&
+        (bookSummaryLabel != nil) &&
+        (bookSummaryText != nil))
+    {
+        // Label Text
+        bookTitle.text          = @"Beautiful Stanger";        // set title of book
+        bookAuthorLabel.text    = @"Author: ";          // set author label
+        bookAuthorText.text     = @"Christina Lauren";   // set author of book
+        bookPubLabel.text       = @"Published: ";
+        bookPubText.text        = @"2013";
+        bookSummaryLabel.text   = @"Summary: ";
+        bookSummaryText.text    = @"Escaping a cheating ex, finance whiz Sara Dillon's moved to New York City and is looking for excitement without a lot of strings attached. So meeting the irresistible, sexy Brit at a dance club should have meant nothing more than a night's fun. But the manner--and speed--with which he melts her inhibitions turns him from a one-time hookup and into her Beautiful Stranger.The whole city knows Max Stella loves women, not that he's ever found one he particularly wants to keep around. Despite pulling in plenty with his Wall Street bad boy charm, it's not until Sara--and the wild photos she lets him take of her--that he starts wondering if there's someone for him outside of the bedroom.Hooking up in places where anybody could catch them, the only thing scarier for Sara than getting caught in public is having Max get too close in private.";
+
     
-    
+        bookTopicsText.text     = bookTopics;
+        
+        // Label Background Colors
+        bookTitle.backgroundColor           = [UIColor blackColor];
+        bookAuthorLabel.backgroundColor     = [UIColor colorWithRed:0.9 green:0.9 blue:0.9 alpha:1] /*#cccccc*/;
+        bookAuthorText.backgroundColor      = [UIColor colorWithRed:0.8 green:0.8 blue:0.8 alpha:1] /*#cccccc*/;
+        bookPubLabel.backgroundColor        = [UIColor colorWithRed:0.9 green:0.9 blue:0.9 alpha:1] /*#cccccc*/;
+        bookPubText.backgroundColor         = [UIColor colorWithRed:0.8 green:0.8 blue:0.8 alpha:1] /*#cccccc*/;
+        bookSummaryLabel.backgroundColor    = [UIColor colorWithRed:0.9 green:0.9 blue:0.9 alpha:1] /*#cccccc*/;
+        bookSummaryText.backgroundColor     = [UIColor colorWithRed:0.8 green:0.8 blue:0.8 alpha:1] /*#cccccc*/;
+        bookTopicsLabel.backgroundColor     = [UIColor colorWithRed:0.9 green:0.9 blue:0.9 alpha:1] /*#cccccc*/;
+        bookTopicsText.backgroundColor      = [UIColor colorWithRed:0.8 green:0.8 blue:0.8 alpha:1] /*#cccccc*/;
+        
+
 }
 
 // this was not auto created, i created this controller
