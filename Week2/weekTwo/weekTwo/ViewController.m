4f5b5b8d2f5cbc54e0d72cdadcc94f833fff7668
Gawain Bracy II <g2musicgroup@fullsail.edu>
2013-04-18 20:39:01 -0700

    Added book topics
    Signed-off-by: Gawain Bracy II <g2musicgroup@fullsail.edu>

--- Week2/weekTwo/weekTwo/ViewController.m
+++ Week2/weekTwo/weekTwo/ViewController.m
@@ -1,23 +1,61 @@
 //
 //  ViewController.m
-//  weekTwo
+//  testApp
 //
-//  Created by Gawain Bracy II on 4/18/13.
+//  Created by Gawain Bracy II on 3/15/13.
 //  Copyright (c) 2013 Gawain Bracy II. All rights reserved.
 //
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface UIViewController ()
 
 @end
 
-@implementation ViewController
+@implementation UIViewController
 
 - (void)viewDidLoad
 {
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
+    
+    // Set BG color of view
+    self.view.backgroundColor = [UIColor colorWithRed:0.8 green:0.8 blue:0.8 alpha:1] /*#cccccc*/;
+}
+
+// this was not auto created, i created this controller
+- (void)viewWillAppear:(BOOL)animated
+{
+    NSArray *bookTopicsArray = [[NSArray alloc] initWithObjects:@"Sara Dillon moces to New York",
+                                @"Meets Brit at a dance club",
+                                @"Sara falls for Brit",
+                                @"Sara meets Max",
+                                @"Max falls for Sara",
+                                nil];
+    NSMutableString *bookTopics = [NSMutableString stringWithCapacity:1];
+    
+    for (int i=0; i<[bookTopicsArray count]; i++)
+    {
+        if (i == [bookTopicsArray count]-1)
+        {
+            [bookTopics appendString:[bookTopicsArray objectAtIndex:i]];
+        } else if ( i < [bookTopicsArray count]-1){
+            [bookTopics appendFormat:@"%@, ", [bookTopicsArray objectAtIndex:i]];
+        } else {
+            break;
+        }
+    };
+    
+    
+    
+    
+}
+
+// this was not auto created, i created this controller
+
+- (void)viewDidAppear:(BOOL)animated
+{
+    [super viewDidAppear:animated];
 }
 
 - (void)didReceiveMemoryWarning
