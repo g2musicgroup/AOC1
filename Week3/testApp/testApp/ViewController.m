57e754abbfcb7725f3f5082f749fb5557bb6ab70
Gawain Bracy II <g2musicgroup@fullsail.edu>
2013-04-25 19:39:03 -0700

    created Add function
    Signed-off-by: Gawain Bracy II <g2musicgroup@fullsail.edu>

--- Week3/testApp/testApp/ViewController.m
+++ Week3/testApp/testApp/ViewController.m
@@ -16,6 +16,19 @@
 
 - (void)viewDidLoad
 {
+    //Call the Add function passing in two integer values. Capture the return of this function into a variable.
+    int sum = [self add:33 with:11];
+    
+    //Bundle the returned integer into an NSNumber and then convert it to a NSString and pass it to the DisplayAlertWithString function.
+    NSNumber *numberSum = [[NSNumber alloc] initWithInt:sum];
+    
+    //Give it text for the title. The message will read, "The number is 00". Replace the 00 with the integer passed into the function.
+    NSString *theNumberText = [NSString stringWithFormat:@"The number is "];
+    NSString *numberToString = [numberSum stringValue];
+    NSString *myNumberAddString = [self append:theNumberText with:numberToString];
+    [self displayAlertWithString:myNumberAddString];
+    
+    
     [super viewDidLoad];
 	// Do any additional setup after loading the view, typically from a nib.
 }
